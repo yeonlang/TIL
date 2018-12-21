e2718ed870aeac6ee294e6156652f8f8b24362fd
Daehyun Kang <naspy001@gmail.com>
2018-12-21 12:22:55 +0900

    day05_2

--- day05/send_message.py
+++ day05/send_message.py
@@ -1,8 +1,11 @@
+import os
 import requests
-chat_id = '758344425'
-token = '706338901:AAE5akaE7mRuQcHXUQmHZbmqAvSbqTrWHyY'
-text = 'stop'
+
+chat_id = os.getenv('CHAT_ID')
+token = os.getenv('TELEGRAM_BOT_TOKEN')
+
 #하고싶은것들
 
+text = 'stop'
 requests.get('https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}?text=오늘의식단')
 
