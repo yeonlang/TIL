1b20f2e633356e88be061f6c4d802bbb7fb5656d
Daehyeon Kang <naspy001@gmail.com>
2019-01-08 12:22:15 +0900

    D05 | 190108 AM | Data structure

---
+++ startcamp/backjoon/sw2005.py
@@ -0,0 +1,5 @@
+n=4
+a=[[0]*i for i in range(1,n+1)]
+a[0][0]=1
+for i in a:
+    
--- startcamp/backjoon/sw5550.py
+++ startcamp/backjoon/sw5550.py
@@ -1,26 +1,36 @@
 T = int(input())
  
 for tc in range(T):
-    n = list(input())
+    n1 = list(input())
+    n=n1[:]
     lst=['c','r','o','a','k']
     c,r,o,a,k = 0,0,0,0,0
-    while n:
-        for i in lst:
-           if i in n:
-                n.remove(i)
-                if i == 'c':
-                    c+=1
-                if i == 'r':
-                    r+=1
-                if i == 'o':
-                    o+=1
-                if i == 'a':
-                    a+=1
-                if i == 'k':
-                    k+=1
+    index, count= -1,0
+    while n1:
+        while n:         
+            for j,i in enumerate(lst):
+                
+                if i in n:
+                    # if n.index(i)<=index :
+                    #     count+=1
+                    index=n.index(i)
+                    if i == 'c':
+                        c+=1
+                    if i == 'r':
+                        r+=1
+                    if i == 'o':
+                        o+=1
+                    if i == 'a':
+                        a+=1
+                    if i == 'k':
+                        k+=1
+                        index=n.index(i)
+                    n1.pop(n1.index(i))
+            if index<=n.index('c'):
+                count+=1        
+            n=n1[n.index('c'):]
+            
     if c == r == o == a ==k:
-        print(c)
+        print(count)
     else :
         print(-1)
-        
-
