13daa1530065264eddd8643ff375fbc8c67bd22e
Daehyun Kang <naspy001@gmail.com>
2019-01-15 23:04:14 +0900

    Desktop | 190115 PM | problems

--- startcamp/backjoon/sw4861.py
+++ startcamp/backjoon/sw4861.py
@@ -1,6 +1,24 @@
-lst=list(input().split())
-for i in range(len(lst)-m+1):
-    find=lst[i:i+m+1]
-    if find==find[::-1] 
-        print(find)
-# n = 10 m = 3 일때 8번 반복되어야한다.
+for tc in range(int(input())):
+    n,m=map(int, input().split())
+    row=[tuple(input()) for i in range(n)]
+    column=list(zip(*row))
+    bools=True
+    for i in range(n):
+        if bools:
+            for j in range(n-m+1):
+                test=row[i][j:m+j]
+                if test == test[::-1] :
+                    print(f'#{tc+1} {"".join(test)}')
+                    bools=False
+                    break
+        else:
+            break
+        if bools:
+            for k in range(n-m+1):
+                test=column[i][k:m+k]
+                if test==test[::-1] :
+                    print(f'#{tc+1} {"".join(test)}')
+                    bools=False
+                    break
+        else:
+            break
