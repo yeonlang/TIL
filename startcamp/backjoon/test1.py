ba1a6e873314b40fa256404e0f7284f6b26c83ef
Daehyeon Kang <naspy001@gmail.com>
2019-01-21 20:00:14 +0900

    test

--- startcamp/backjoon/test1.py
+++ startcamp/backjoon/test1.py
@@ -1,10 +1,1 @@
-from collections import Counter 
-
-case=int(input())
-for i in range(1,case+1):
-    testnum=int(input())
-    test=list(map(int, input().split()))
-    
-    cnt = Counter(test)
-    num=cnt.most_common()
-print(num)
+print('hi')
